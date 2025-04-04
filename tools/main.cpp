b3fae5fe33cd6832fe3d5de7a3e81403fa6d41eb
Richard <legalize@xmission.com>
2025-04-03 22:06:30 -0600

    Add menu item to toggle display of line number margin; rename classes

--- tools/main.cpp
+++ tools/main.cpp
@@ -4,51 +4,71 @@
 #include <wx/stc/stc.h>
 #include <wx/wx.h>
 
-class MyApp : public wxApp
+enum class MarginIndex
+{
+    LINE_NUMBER = 0,
+};
+inline int operator+(MarginIndex value)
+{
+    return static_cast<int>(value);
+}
+
+class ScintillaApp : public wxApp
 {
 public:
     virtual bool OnInit();
 };
 
-class MyFrame : public wxFrame
+class ScintillaFrame : public wxFrame
 {
 public:
-    MyFrame(const wxString& title);
+    ScintillaFrame(const wxString& title);
 
 private:
     void load_language_theme();
-    void on_exit(wxCommandEvent& event);
+    void on_exit(wxCommandEvent &event);
+    void show_hide_line_numbers();
+    void on_view_line_numbers(wxCommandEvent &event);
 
+    wxMenuItem *m_view_lines{};
     wxStyledTextCtrl *m_stc{};
+    int m_line_margin_width{};
+    bool m_show_lines{};
 };
 
-wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP(ScintillaApp);
 
-bool MyApp::OnInit()
+bool ScintillaApp::OnInit()
 {
-    MyFrame *frame = new MyFrame("Scintilla Editing Example");
+    ScintillaFrame *frame = new ScintillaFrame("Scintilla Editing Example");
     frame->Show(true);
     return true;
 }
 
-MyFrame::MyFrame(const wxString &title) :
+ScintillaFrame::ScintillaFrame(const wxString &title) :
     wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(800, 600))
 {
-    wxMenuBar* menuBar = new wxMenuBar;
-    wxMenu* fileMenu = new wxMenu;
-    fileMenu->Append(wxID_EXIT, "&Quit\tAlt-F4", "Quit");
-    menuBar->Append(fileMenu, "&File");
-    wxFrameBase::SetMenuBar(menuBar);
-    Bind(wxEVT_MENU, &MyFrame::on_exit, this, wxID_EXIT);
+    wxMenuBar *menu_bar = new wxMenuBar;
+    wxMenu *file = new wxMenu;
+    file->Append(wxID_EXIT, "&Quit\tAlt-F4", "Quit");
+    menu_bar->Append(file, "&File");
+    wxMenu *view = new wxMenu;
+    m_view_lines = view->Append(wxID_ANY, "&Line Numbers", "Line Numbers", wxITEM_CHECK);
+    menu_bar->Append(view, "&View");
+    Bind(wxEVT_MENU, &ScintillaFrame::on_view_line_numbers, this, m_view_lines->GetId());
+    wxFrameBase::SetMenuBar(menu_bar);
+    Bind(wxEVT_MENU, &ScintillaFrame::on_exit, this, wxID_EXIT);
 
     m_stc = new wxStyledTextCtrl(this, wxID_ANY);
     m_stc->LoadLexerLibrary(wxT("./formula-lexer") + wxDynamicLibrary::GetDllExt(wxDL_LIBRARY));
     m_stc->SetLexerLanguage(wxT("id-formula"));
     load_language_theme();
     m_stc->Colourise(0, -1);
+
+    show_hide_line_numbers();
 }
 
-void MyFrame::load_language_theme()
+void ScintillaFrame::load_language_theme()
 {
     wxFont typewriter;
     typewriter.SetFamily(wxFONTFAMILY_TELETYPE);
@@ -71,9 +91,24 @@ void MyFrame::load_language_theme()
     wxColor identifier;
     wxASSERT(wxFromString("purple", &identifier));
     m_stc->StyleSetForeground(+formula::Syntax::IDENTIFIER, identifier);
+    m_stc->SetMarginType(+MarginIndex::LINE_NUMBER, wxSTC_MARGIN_NUMBER);
+    m_stc->SetMarginWidth(+MarginIndex::LINE_NUMBER, 0);
+    m_line_margin_width = m_stc->TextWidth(wxSTC_STYLE_LINENUMBER, "_99999");
 }
 
-void MyFrame::on_exit(wxCommandEvent &/*event*/)
+void ScintillaFrame::on_exit(wxCommandEvent & /*event*/)
 {
     Close(true);
+}
+
+void ScintillaFrame::show_hide_line_numbers()
+{
+    m_stc->SetMarginWidth(+MarginIndex::LINE_NUMBER, m_show_lines ? m_line_margin_width : 0);
+    m_view_lines->Check(m_show_lines);
+}
+
+void ScintillaFrame::on_view_line_numbers(wxCommandEvent &event)
+{
+    m_show_lines = !m_show_lines;
+    show_hide_line_numbers();
 }
